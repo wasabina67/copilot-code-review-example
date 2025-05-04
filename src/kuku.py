906b3fc43105f9f02aa04cf6da41a04f195f66a7
wasabina67 <wasabina67@gmail.com>
2025-05-04 16:13:39 +0900

    Update src/kuku.py

--- src/kuku.py
+++ src/kuku.py
@@ -1,6 +1,6 @@
 def kuku():
     for i in range(1, 10):
-        print(" ".join([f"{i}*{j}={i*j}" for j in range(1, 10)]))
+        print(" ".join([f"{i*j}" for j in range(1, 10)]))
 
 
 if __name__ == "__main__":
